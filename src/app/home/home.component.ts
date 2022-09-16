75724bc31403e4f8e9489b7e0e5674553423e1ef
shex003 <eng.shex003@gmail.com>
2022-09-16 13:55:00 +0200

    final

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,20 +14,20 @@ export class HomeComponent implements OnInit {
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    $('#bg-particles').particleground({
-      // dotColor: 'rgb(101, 236, 236)',
-      // lineColor: '#D3959B',
-      dotColor: '#fff',
-      lineColor: '#fff',
-      minSpeedX: '0.1',
-      minSpeedY: '0.1',
-      maxSpeedX: '0.6',
-      maxSpeedY: '0.5',
-      directionY: 'up',
-      density: '9000',
-      particleRadius: '4',
-      parallaxMultiplier: '4',
-    });
+    // $('#bg-particles').particleground({
+    //   // dotColor: 'rgb(101, 236, 236)',
+    //   // lineColor: '#D3959B',
+    //   dotColor: '#fff',
+    //   lineColor: '#fff',
+    //   minSpeedX: '0.1',
+    //   minSpeedY: '0.1',
+    //   maxSpeedX: '0.6',
+    //   maxSpeedY: '0.5',
+    //   directionY: 'up',
+    //   density: '9000',
+    //   particleRadius: '4',
+    //   parallaxMultiplier: '4',
+    // });
   }
   onSubmit(form: NgForm) {
     this.isSubmitted = true;
